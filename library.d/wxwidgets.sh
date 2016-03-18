96373cc3e775c3a4792ba856fc6b6132917d1b54
zer0 <osom8979@gmail.com>
2016-03-18 14:36:10 +0900

    Create a wxWidgets build script.

---
+++ library.d/wxwidgets.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+if [[ -z $TPARTY_HOME ]]; then
+    echo 'Not defined TPARTY_HOME variable.'
+    exit 1
+fi
+
+TPARTY_LOCAL=$TPARTY_HOME/local
+TPARTY_TMP=$TPARTY_HOME/tmp
+
+## Don't remove DEPENDENCIES variable.
+DEPENDENCIES=
+
+NAME='wxWidgets-3.1.0'
+URL='https://codeload.github.com/wxWidgets/wxWidgets/tar.gz/v3.1.0'
+MD5='6d2af648c5d0b2d366e7050d06b9a89f'
+TEMP_DIR="$TPARTY_TMP/build"
+DEST_NAME="$NAME.tar.gz"
+WORK_NAME="$NAME"
+ALREADY="$TPARTY_LOCAL/include/wx-3.1/wx/config.h"
+LOG_PATH="$TEMP_DIR/$NAME-`datetime`.log"
+
+function runLinux {
+    code=$?; [[ $code != 0 ]] && exit $code
+    ./configure --prefix=$TPARTY_LOCAL >> $LOG_PATH
+
+    code=$?; [[ $code != 0 ]] && exit $code
+    make >> $LOG_PATH
+
+    code=$?; [[ $code != 0 ]] && exit $code
+    make install >> $LOG_PATH
+}
+
+LINUX_FUNC=runLinux
+MACOSX_FUNC=runLinux
+WINDOWS_FUNC=runLinux
+
+. general-build "$NAME" "$URL" "$MD5" \
+    "$TEMP_DIR" "$DEST_NAME" "$WORK_NAME" "$ALREADY" "$LOG_PATH" \
+    "$LINUX_FUNC" "$MACOSX_FUNC" "$WINDOWS_FUNC"
+
