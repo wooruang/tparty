9d1c9d933f9d5116664ff25969c993e7c5f0b0e8
zer0 <osom8979@gmail.com>
2016-03-18 12:52:23 +0900

    Add a export variables.

--- install.sh
+++ install.sh
@@ -9,16 +9,19 @@ function getScriptDirectory {
 
 if [[ -z $TPARTY_HOME ]]; then
     # Not found TPARTY_HOME variable.
-    TPARTY_HOME=`getScriptDirectory`
+    export TPARTY_HOME=`getScriptDirectory`
 fi
 
 WORKING=$PWD
 BIN_DIR=$TPARTY_HOME/bin
 INSTALL_DIR=$TPARTY_HOME/library.d
-PATH=$BIN_DIR:$PATH
+LOCAL_DIR=$TPARTY_HOME/local
 
-export PATH
-export TPARTY_HOME
+export PATH=$BIN_DIR:$LOCAL_DIR/bin:$PATH
+export CPATH=$LOCAL_DIR/include:$CPATH
+export LIBRARY_PATH=$LOCAL_DIR/lib:$LIBRARY_PATH
+export LD_LIBRARY_PATH=$LOCAL_DIR/lib:$LD_LIBRARY_PATH
+export DYLD_LIBRARY_PATH=$LOCAL_DIR/lib:$DYLD_LIBRARY_PATH
 
 ## Warning: Don't use the quoting("...").
 for cursor in $INSTALL_DIR/*.sh; do
