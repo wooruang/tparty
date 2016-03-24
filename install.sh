b52163e8d7a8ebf92fe1b043d27480a85764ac61
zer0 <osom8979@gmail.com>
2016-03-24 16:53:55 +0900

    Create a cache file.

--- install.sh
+++ install.sh
@@ -23,16 +23,65 @@ export LIBRARY_PATH=$LOCAL_DIR/lib:$LIBRARY_PATH
 export LD_LIBRARY_PATH=$LOCAL_DIR/lib:$LD_LIBRARY_PATH
 #export DYLD_LIBRARY_PATH=$LOCAL_DIR/lib:$DYLD_LIBRARY_PATH
 
+# Create a cache.
+CACHE=$TPARTY_HOME/cache
+if [[ ! -f "$CACHE" ]]; then
+    ## Warning: Don't use the quoting("...").
+    for cursor in $INSTALL_DIR/*.sh; do
+        echo ${cursor##*/} >> $CACHE
+    done
+
+    echo "Not found cache file."
+    echo "Create cache file: $CACHE"
+    exit 0
+fi
+
+# Check install files.
+INSTALL_SCRIPTS=''
+for cursor in $(cat $CACHE); do
+    current_script="$INSTALL_DIR/$cursor"
+    if [[ -f "$current_script" ]]; then
+        if [[ "$INSTALL_SCRIPTS" == "" ]]; then
+            INSTALL_SCRIPTS="$cursor"
+        else
+            INSTALL_SCRIPTS="$INSTALL_SCRIPTS,$cursor"
+        fi
+    fi
+done
+
+# User input.
+echo "Install scripts: $INSTALL_SCRIPTS"
+read -p "Do you want to continue [y/n]?" yn
+case $yn in
+[Yy]*)
+    # Continue!!
+    ;;
+[Nn]*)
+    exit 1
+    ;;
+*)
+    echo "Please answer Yes or No."
+    exit 1
+    ;;
+esac
+
 ## Warning: Don't use the quoting("...").
-for cursor in $INSTALL_DIR/*.sh; do
-    echo 'Install:' $cursor
-    source $cursor
+for cursor in $(cat $CACHE); do
+    current_script="$INSTALL_DIR/$cursor"
+    if [[ ! -f "$current_script" ]]; then
+        continue
+    fi
+
+    echo "Install: $current_script"
+    source "$current_script"
     code=$?
 
     if [[ $code == 0 ]]; then
         echo 'Install success.'
     else
         echo 'Install failure.'
+        # Don't use exit command.
+        #exit 0
     fi
 done
 
