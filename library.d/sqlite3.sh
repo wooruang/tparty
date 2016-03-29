534b4a0feaeb7e23f957a7ec04d71e22f8cf0bd4
zer0 <osom8979@gmail.com>
2016-03-29 11:24:14 +0900

    Create a SQLite3 build script.

---
+++ library.d/sqlite3.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+if [[ -z $TPARTY_HOME ]]; then
+    echo 'Not defined TPARTY_HOME variable.'
+    exit 1
+fi
+
+TPARTY_LOCAL=$TPARTY_HOME/local
+TPARTY_TMP=$TPARTY_HOME/tmp
+
+## Don't remove DEPENDENCIES variable.
+DEPENDENCIES=
+build-dependency $DEPENDENCIES
+
+NAME='sqlite-autoconf-3110100'
+URL='https://www.sqlite.org/2016/sqlite-autoconf-3110100.tar.gz'
+MD5='c66b4e9d5ca6cfd15471b5b03fc44537'
+TEMP_DIR="$TPARTY_TMP/build"
+DEST_NAME="$NAME.tar.gz"
+WORK_NAME="$NAME"
+ALREADY="$TPARTY_LOCAL/lib/libsqlite3.a"
+LOG_PATH="$TEMP_DIR/$NAME-`datetime`.log"
+THREAD_FLAG=`thread-flag`
+
+function runCommon {
+    code=$?; [[ $code != 0 ]] && exit $code
+    ./configure --prefix=$TPARTY_LOCAL >> $LOG_PATH
+
+    code=$?; [[ $code != 0 ]] && exit $code
+    make >> $LOG_PATH
+
+    code=$?; [[ $code != 0 ]] && exit $code
+    make install >> $LOG_PATH
+}
+
+LINUX_FUNC=runCommon
+MACOSX_FUNC=runCommon
+WINDOWS_FUNC=runCommon
+
+. general-build "$NAME" "$URL" "$MD5" "$TEMP_DIR"    \
+    "$DEST_NAME" "$WORK_NAME" "$ALREADY" "$LOG_PATH" \
+    "$LINUX_FUNC" "$MACOSX_FUNC" "$WINDOWS_FUNC"
+
